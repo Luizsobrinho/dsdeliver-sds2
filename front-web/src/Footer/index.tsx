9fc267ace3f945173979980abe2021175017c86f
José Luiz dos Santos Sobrinho <luizsobrinho3000@hotmail.com>
2021-01-09 21:55:15 -0300

    Add routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -9,7 +9,7 @@ function Footer() {
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
                 <a href="https://www.youtube.com/c/DevSuperior" target="_new"> <YoutubeIcon /> </a>
-                <a href="#" target="_new"> <LinkedinIcon /> </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new"> <LinkedinIcon /> </a>
                 <a href="https://www.instagram.com/devsuperior.ig/" target="_new"> <InstagramIcon /></a>
             </div>
         </footer>
